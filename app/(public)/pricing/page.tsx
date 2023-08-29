7d76a2e3ca21b81b25d9daed5a227b909773f608
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2023-08-28 23:50:56 -0700

    Link to products and pricing pages

---
+++ app/(public)/pricing/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../page.module.css'
+import ResponsiveAppBar from '../responsive-app-bar'
+
+export default function Pricing() {
+  return (
+    <main className={styles.main}>
+      <ResponsiveAppBar />
+      Pricing Page
+    </main>
+  )
+}
---
+++ app/(public)/products/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../page.module.css'
+import ResponsiveAppBar from '../responsive-app-bar'
+
+export default function Products() {
+  return (
+    <main className={styles.main}>
+      <ResponsiveAppBar />
+      Products Page
+    </main>
+  )
+}
