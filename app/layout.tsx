813611f09623153caf95e283ba0b3ee2d38134e0
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2023-08-28 20:33:19 -0700

    Added CssBaseline

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import CssBaseline from '@mui/material/CssBaseline'
+
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <CssBaseline />
       <body className={inter.className}>{children}</body>
     </html>
   )
